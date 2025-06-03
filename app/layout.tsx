17b5b24b49019c39f79491d71d76ae79bf9d51a6
Raiyan Hossain <163441987+raiiyan@users.noreply.github.com>
2025-06-04 05:38:48 +0600

    up3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
